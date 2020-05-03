b07e974bfe62956431ff97f6bc49da1eb285fdad
Ninjalemur <ninjalemur33@gmail.com>
2020-05-03 09:33:46 +0800

    Create enable_apis.sh

---
+++ enable_apis.sh
@@ -0,0 +1,15 @@
+echo "Compute Engine API: enabling"
+gcloud services enable compute.googleapis.com
+echo "Compute Engine API: enabled"
+echo "App Engine API: enabling"
+gcloud services enable appengine.googleapis.com
+echo "App Engine API: enabled"
+echo "Cloud Functions API: enabling"
+gcloud services enable cloudfunctions.googleapis.com
+echo "Cloud Functions API: enabled"
+echo "Sheets API: enabling"
+gcloud services enable sheets.googleapis.com
+echo "Sheets API: enabled"
+echo "Cloud Build API: enabling"
+gcloud services enable cloudbuild.googleapis.com
+echo "Cloud Build API: enabled"
